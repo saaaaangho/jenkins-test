c09ebf3529c17d60bb76aab0ad657c4611081bd0
root <root@shlee-test.novalocal>
2025-05-30 10:24:12 +0900

    jenkins-test 10:24

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ Flask 서버가 정상적으로 동작 중입니다!"
+    return "Flask Server is running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
